8b5416c99ccec2d079dee0a784549046b07aaae8
Koenraad <git@alias.zweerts.xyz>
2024-09-26 12:45:08 +0200

    Log some values

--- functions/print.js
+++ functions/print.js
@@ -1,5 +1,4 @@
 export async function onRequest(context) {
-  const resume = await env.ASSETS.fetch('resume');
-  console.log(context);
-  console.log(resume);
+  const request = await env.ASSETS.fetch(request);
+  console.log(request);
 }
